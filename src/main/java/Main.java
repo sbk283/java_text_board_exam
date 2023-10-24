468d04274fd0911c780e630a5df1752b8a8fc108
wasabikun1704 <sbk283@naver.com>
2023-10-24 13:30:21 +0900

    text_board

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI");
+        System.out.println("== 프로그램 실행 ==");
+        Scanner sc = new Scanner(System.in);
+//            List<> = new ArrayList<>();
+
+        while (true) {
+            System.out.print("명령 >> ");
+            String command = sc.nextLine();
+            if (command.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (command.equals("등록")) {
+
+            } else if (command.equals("수정")) {
+
+            } else if (command.equals("삭제")) {
+
+            }
+        }
     }
 }
